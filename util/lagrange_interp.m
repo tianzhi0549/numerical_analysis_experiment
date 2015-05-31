6438764ca337650edda1ca2455d7896d1052a69a
Tian Zhi <tianzhi0549@163.com>
2015-05-31 13:04:42 +0800

    改为可以直接计算向量输入。

--- util/lagrange_interp.m
+++ util/lagrange_interp.m
@@ -2,16 +2,27 @@
 % X -已知点的x坐标。
 % Y -已知点的y坐标。
 % x -需要通过插值计算y的x的值。
-function y = lagrange_interp(X, Y, x)
-    y=0;
+function interp_y= lagrange_interp(X, Y, interp_x)
+    interp_y=zeros(1, size(interp_x, 2));
     points_count=size(X, 2);
-    for k=1:points_count
-        l=1;
-        for i=1:points_count
-            if i~=k
-                l=l*(x-X(i))/(X(k)-X(i));
+    coefficients=ones(1, points_count);
+        for k=1:points_count
+            % 计算第k个基函数的系数。
+            for i=1:points_count
+                if i~=k
+                    coefficients(k)=coefficients(k)*(X(k)-X(i));
+                end
+            end
+            coefficients(k)=Y(k)/coefficients(k);
+            % 计算每个插值点的y加上第k个基函数的值。
+            for index_interp=1:size(interp_x, 2)
+                l=1;
+                for i=1:points_count
+                    if i~=k
+                        l=l*(interp_x(index_interp)-X(i));
+                    end
+                end
+                interp_y(index_interp)=interp_y(index_interp)+coefficients(k)*l;
             end
         end
-        y=y+Y(k)*l;
-    end
 end
