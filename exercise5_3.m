f42a3c616a8ea09e2ec33062de6b934674536b04
Tian Zhi <tianzhi0549@163.com>
2015-06-22 01:51:22 +0800

    add comments.

--- exercise5_3.m
+++ exercise5_3.m
@@ -1,9 +1,13 @@
+% 矩阵阶数。
 n=5;
-M=gen_rnd_tridiagonal_mat(n);
+% 生成系数矩阵A.
+A=gen_rnd_tridiagonal_mat(n);
+% 生成右端向量b.
 b=randn(n, 1);
-disp('M=');
-disp(M);
+disp('A=');
+disp(A);
 disp('b=');
 disp(b);
 disp('x=');
+% 使用“追赶法”求解并打印结果。
 disp(thomas_algorithm(M, b));
