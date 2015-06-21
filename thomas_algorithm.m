5262340635b9b5e0acd48649b67fdb94c8ad18fd
Tian Zhi <tianzhi0549@163.com>
2015-06-21 22:45:08 +0800

    fixed bug.

--- thomas_algorithm.m
+++ thomas_algorithm.m
@@ -1,15 +1,25 @@
-function x=thomas_algorithm(A, b)
+function x=thomas_algorithm(A, f)
 % “追赶法”求解对角严格占优三对角矩阵。
+    function v=b(i)
+        v=A(i, i);
+    end
+    function v=c(i)
+        v=A(i, i+1);
+    end
+    function v=a(i)
+        v=A(i, i-1);
+    end
+
     n=length(A);
     beta=zeros(n-1, 1);
-    beta(1)=A(1, 2)/A(1, 1);
-	for i=2:n-1
-        beta(i)=A(i-1, i)/(A(i, i)-A(i, i-1)*beta(i-1));
+    beta(1)=c(1)/b(1);
+    for i=2:n-1
+        beta(i)=c(i)/(b(i)-a(i)*beta(i-1));
     end
     y=zeros(n, 1);
-    y(1)=b(1)/A(1, 1);
+    y(1)=f(1)/b(1);
     for i=2:n
-        y(i)=(b(i)-A(i, i-1)*y(i-1))/(A(i, i)-A(i, i-1)*beta(i-1));
+        y(i)=(f(i)-a(i)*y(i-1))/(b(i)-a(i)*beta(i-1));
     end
     x=zeros(n, 1);
     x(n)=y(n);
